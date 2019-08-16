5ea819faaa1bee9e807074cdd9944fb50f619628
Lisa Ngo <lisango.e@gmail.com>
2019-08-16 11:59:38 +0200

    heroky

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #   get 'cocktails', to: 'cocktails#index'
   #   get 'cocktail', to: 'cocktails#show'
+  root to: "cocktails#index"
   resources :cocktails, only: [:new, :create, :show, :index] do
     resources :doses, only: [:new, :create, :show]
   end
