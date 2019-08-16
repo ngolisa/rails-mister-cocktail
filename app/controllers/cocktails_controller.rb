2a09777399c712f77876209f92f04ae601188222
Lisa Ngo <lisango.e@gmail.com>
2019-08-16 15:12:33 +0200

    add delete to cocktail

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -22,6 +22,8 @@ class CocktailsController < ApplicationController
 
   def destroy
     @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+    redirect_to cocktails_path
   end
 
   private
